ac4292ff7508aeb0bf134f0bc4bebea35c3866b2
Ana Clara <ana4ew.br@gmail.com>
2024-10-03 20:35:00 -0300

    Updated: Archives' name

--- src/main/java/com/example/expense_control/domain/entity/enums/Category.java
+++
@@ -1,5 +0,0 @@
-package com.example.expense_control.domain.entity.enums;
-
-public enum Category {
-    UNDEFINED;
-}
--- src/main/java/com/example/expense_control/domain/entity/enums/Status.java
+++
@@ -1,7 +0,0 @@
-package com.example.expense_control.domain.entity.enums;
-
-public enum Status {
-    ACTIVE,
-    INACTIVE,
-    DELETED;
-}
