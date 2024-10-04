990f9547787a819c47f8c776415c00abf73abf18
Ana Clara <ana4ew.br@gmail.com>
2024-10-03 21:23:32 -0300

    Updated: Product Entity

--- src/main/java/com/example/web_inventory/domain/entity/ProductEntity.java
+++ src/main/java/com/example/web_inventory/domain/entity/ProductEntity.java
@@ -14,12 +14,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 @Table(name="products")
 @Entity(name="products")
 @Data
-@EqualsAndHashCode(of="id")
 public class ProductEntity {
     
     @Id
