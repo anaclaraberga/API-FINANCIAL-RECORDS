d68b24c9e9237a534adcae7de2ebd073291f5b7d
Ana Clara <ana4ew.br@gmail.com>
2024-10-03 21:22:08 -0300

    Created: Client Entity

---
+++ src/main/java/com/example/web_inventory/domain/entity/ClientEntity.java
@@ -0,0 +1,21 @@
+package com.example.web_inventory.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity(name = "clients")
+@Table(name = "clients")
+@Data
+public class ClientEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+}
