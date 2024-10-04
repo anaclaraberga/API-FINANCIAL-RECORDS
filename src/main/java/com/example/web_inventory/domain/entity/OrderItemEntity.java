4bf8d5760bacc1fde21785abd7212bfd15b1dd4a
Ana Clara <ana4ew.br@gmail.com>
2024-10-04 16:21:47 -0300

    Created: Order Item Entity

---
+++ src/main/java/com/example/web_inventory/domain/entity/OrderItemEntity.java
@@ -0,0 +1,20 @@
+package com.example.web_inventory.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Table(name="order-items")
+@Entity(name="order-items")
+@Data
+public class OrderItemEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    
+}
