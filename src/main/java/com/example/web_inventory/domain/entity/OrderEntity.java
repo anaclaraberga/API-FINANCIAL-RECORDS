47a94ed6295795e69e84055da6003a010fba9972
Ana Clara <ana4ew.br@gmail.com>
2024-10-04 16:15:24 -0300

    Updated: Order entity

--- src/main/java/com/example/web_inventory/domain/entity/OrderEntity.java
+++ src/main/java/com/example/web_inventory/domain/entity/OrderEntity.java
@@ -1,6 +1,14 @@
 package com.example.web_inventory.domain.entity;
 
+import javax.print.attribute.standard.DateTimeAtCreation;
+
+import com.example.web_inventory.domain.entity.enums.Status;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -9,4 +17,16 @@ import lombok.Data;
 @Data
 public class OrderEntity {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false, name = "date_time_at_creation")
+    private DateTimeAtCreation dateTimeAtCreation;
+
+    @Column(nullable = false)
+    private CustomerEntity customer;
+
+    @Column(nullable = false)
+    private Status status;
 }
