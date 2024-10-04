e383496e14cedf570bdccf5035e4ac1e47650d33
Ana Clara <ana4ew.br@gmail.com>
2024-10-03 21:22:26 -0300

    Created: Order Entity

---
+++ src/main/java/com/example/web_inventory/domain/entity/OrderEntity.java
@@ -0,0 +1,12 @@
+package com.example.web_inventory.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity(name = "orders")
+@Table(name = "orders")
+@Data
+public class OrderEntity {
+    
+}
