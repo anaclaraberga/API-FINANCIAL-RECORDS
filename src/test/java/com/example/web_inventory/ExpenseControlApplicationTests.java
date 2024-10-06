1a459324994540eb04f1151ad304e5a83c51950a
Ana Clara <ana4ew.br@gmail.com>
2024-10-06 20:14:03 -0300

    Updated: Files' name

--- src/test/java/com/example/web_inventory/ExpenseControlApplicationTests.java
+++ src/test/java/com/example/web_inventory/ExpenseControlApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.expense_control;
+package com.example.web_inventory;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ExpenseControlApplicationTests {
+class WebInventoryApplicationTests {
 
 	@Test
 	void contextLoads() {
