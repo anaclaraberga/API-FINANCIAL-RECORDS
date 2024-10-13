4e052da3d1b7b17b975e4254ad605647bdb13af0
Ana Clara <ana4ew.br@gmail.com>
2024-10-13 18:52:36 -0300

    Adjusted: Annotations organization

--- src/main/java/com/example/web_inventory/entities/Customer.java
+++ src/main/java/com/example/web_inventory/entities/Customer.java
@@ -6,11 +6,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity(name = "customers")
 @Table(name = "customers")
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
 public class Customer {
     
     @Id
