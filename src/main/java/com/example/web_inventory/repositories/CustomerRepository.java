316fe39fa6abdaaeb65c716121e5dd279c13b02c
Ana Clara <ana4ew.br@gmail.com>
2024-10-13 18:53:02 -0300

    Created: Customer repository

--- src/main/java/com/example/web_inventory/repositories/CustomerRepository.java
+++ src/main/java/com/example/web_inventory/repositories/CustomerRepository.java
@@ -1,9 +1,9 @@
 package com.example.web_inventory.repositories;
 
-public interface CustomerRepository {
-  // CustomerBO save(CustomerBO bo);
-  // Optional<CustomerBO> findById(Long id);
-  // Optional<CustomerBO> findByEmail(String email);
-  // void deleteById(Long id);
-  // CustomerBO update(CustomerBO bo);
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.web_inventory.entities.Customer;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+ 
 }
