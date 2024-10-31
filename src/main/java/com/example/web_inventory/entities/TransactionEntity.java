3738ec5ecc44e6d8701d0daae4c5cd123d0c9ee9
Ana Clara <ana4ew.br@gmail.com>
2024-10-30 21:55:07 -0300

    refactor: transaction entity

--- src/main/java/com/example/web_inventory/entities/TransactionEntity.java
+++ src/main/java/com/example/web_inventory/entities/TransactionEntity.java
@@ -2,9 +2,9 @@ package com.example.web_inventory.entities;
 
 import java.io.Serializable;
 import java.math.BigInteger;
+import java.time.LocalDateTime;
 
-import javax.print.attribute.standard.DateTimeAtCreation;
-
+import com.example.web_inventory.dtos.request.TransactionRequestDTO;
 import com.example.web_inventory.enums.PaymentMethod;
 
 import jakarta.persistence.Column;
@@ -15,11 +15,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Table(name="transactions")
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TransactionEntity implements Serializable{
     
     @Id
@@ -28,14 +32,20 @@ public class TransactionEntity implements Serializable{
 
     @OneToOne
     @JoinColumn(name = "order_id", nullable = false)
-    private OrderEntity order;
+    private OrderEntity orderId;
 
     @Column(nullable = false, name = "date_time_at_creation")
-    private DateTimeAtCreation dateTimeAtCreation;
+    private LocalDateTime dateTimeAtCreation;
 
     @Column(nullable = false)
     private BigInteger amount;
 
     @Column(nullable = false)
     private PaymentMethod paymentMethod;
+
+    public TransactionEntity(TransactionRequestDTO dto) {
+        this.amount = dto.getAmount();
+        this.paymentMethod = dto.getPaymentMethod();
+        this.orderId = dto.getOrderId();
+    }
 }
