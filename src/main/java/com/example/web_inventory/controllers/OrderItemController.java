1801f205d10c2ac78221dd4a0beadcc24db993de
Ana Clara <ana4ew.br@gmail.com>
2024-11-12 21:57:19 -0300

    Created: Order Item Controller + Service

---
+++ src/main/java/com/example/web_inventory/controllers/OrderItemController.java
@@ -0,0 +1,78 @@
+package com.example.web_inventory.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.web_inventory.dtos.request.OrderItemRequestDTO;
+import com.example.web_inventory.dtos.response.OrderItemResponseDTO;
+import com.example.web_inventory.entities.OrderItemEntity;
+import com.example.web_inventory.services.OrderItemService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/product")
+@Slf4j
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class OrderItemController {
+    
+    @Autowired
+    OrderItemService orderItemService;
+
+    public OrderItemController(OrderItemService orderItemService) {
+
+        this.orderItemService = orderItemService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<OrderItemResponseDTO> createOrderItem(@RequestBody OrderItemRequestDTO dto) {
+
+        OrderItemEntity orderItem = orderItemService.createOrderItem(dto);
+
+        return ResponseEntity.status(201).body(new OrderItemResponseDTO(orderItem));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderItemEntity> findOrderItemById(@PathVariable Long id) {
+
+        OrderItemEntity entity = this.orderItemService.findOrderItemById(id);
+
+        return ResponseEntity.ok(entity);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<OrderItemEntity> getAllOrderItems() {
+
+        List<OrderItemEntity> orderItem = orderItemService.getAllOrderItems();
+
+        return orderItem;
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<OrderItemEntity> updateOrderItemById(@PathVariable(value = "id") Long id, @RequestBody OrderItemRequestDTO dto) {
+
+        return orderItemService.updateOrderItemById(dto, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteOrderItemById(@PathVariable("id") Long id) {
+
+        return orderItemService.deleteById(id);
+    }
+}
---
+++ src/main/java/com/example/web_inventory/services/OrderItemService.java
@@ -0,0 +1,69 @@
+package com.example.web_inventory.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.example.web_inventory.dtos.request.OrderItemRequestDTO;
+import com.example.web_inventory.entities.OrderItemEntity;
+import com.example.web_inventory.entities.ProductEntity;
+import com.example.web_inventory.repositories.OrderItemRepository;
+import com.example.web_inventory.repositories.ProductRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class OrderItemService {
+    
+    @Autowired
+    private OrderItemRepository repository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public OrderItemEntity createOrderItem(OrderItemRequestDTO dto) {
+        ProductEntity product = productRepository.findById(dto.getOrderId())
+            .orElseThrow(() -> new RuntimeException("Product not found."));
+        
+        OrderItemEntity entity = new OrderItemEntity(dto);
+
+        entity.setProductId(product);
+
+        return repository.save(entity);
+    }
+
+    public OrderItemEntity findOrderItemById (Long id) {
+        Optional<OrderItemEntity> product = this.repository.findById(id);
+
+        return product.orElseThrow(() -> new ObjectNotFoundException("Itens de pedido" + id + "Tipo: " + OrderItemEntity.class.getName(), product));
+    }
+
+    public List<OrderItemEntity> getAllOrderItems() {
+
+        return repository.findAll();
+    }
+
+    public ResponseEntity<OrderItemEntity> updateOrderItemById(OrderItemRequestDTO dto, Long id) {
+        return repository.findById(id)
+        .map(update -> {
+            update.setQuantity(dto.getQuantity());
+            update.setUnitPrice(dto.getUnitPrice());
+
+            OrderItemEntity updated = repository.save(update);
+            return ResponseEntity.ok().body(updated);
+        }).orElse(ResponseEntity.notFound().build());
+    }
+
+    public ResponseEntity<Object> deleteById (Long id) {
+        return repository.findById(id)
+        .map(delete -> {
+            repository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }).orElse(ResponseEntity.notFound().build());
+    }
+}
