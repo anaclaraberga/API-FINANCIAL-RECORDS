16cebbf01c11844eed9b907a9c44f36431edb462
Ana Clara <ana4ew.br@gmail.com>
2024-11-14 16:41:16 -0300

    Fixed: Status getter and setter

--- src/main/java/com/example/web_inventory/dtos/request/OrderRequestDTO.java
+++ src/main/java/com/example/web_inventory/dtos/request/OrderRequestDTO.java
@@ -2,6 +2,8 @@ package com.example.web_inventory.dtos.request;
 
 import java.time.LocalDateTime;
 
+import com.example.web_inventory.enums.Status;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,4 +13,13 @@ public class OrderRequestDTO {
     private LocalDateTime dateTimeAtCreation;
     private Long customerId;
     private String status;
+
+    public Status getStatus() {
+
+        return Status.valueOf(this.status);
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
