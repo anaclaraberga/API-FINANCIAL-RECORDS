a62b34d917e34bacb8b7d8d96a1efb12e9ad5ead
Ana Clara <ana4ew.br@gmail.com>
2024-11-15 16:14:22 -0300

    Fixed: Status import

--- src/main/java/com/example/web_inventory/dtos/response/OrderResponseDTO.java
+++ src/main/java/com/example/web_inventory/dtos/response/OrderResponseDTO.java
@@ -2,9 +2,8 @@ package com.example.web_inventory.dtos.response;
 
 import java.time.LocalDateTime;
 
-import javax.net.ssl.SSLEngineResult.Status;
-
 import com.example.web_inventory.entities.OrderEntity;
+import com.example.web_inventory.enums.Status;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -15,20 +14,12 @@ public class OrderResponseDTO {
     private Long id;
     private LocalDateTime dateTimeAtCreation;
     private Long customerId;
-    private String status;
-
-    public Status getStatus() {
-
-        return Status.valueOf(this.status);
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
+    private Status status;
 
     public OrderResponseDTO(OrderEntity entity) {
         this.id = entity.getId();
         this.dateTimeAtCreation = entity.getDateTimeAtCreation();
         this.customerId = entity.getCustomerId().getId();
+        this.status = entity.getStatus();
     }
 }
