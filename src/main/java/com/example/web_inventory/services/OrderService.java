4930d50ac94214a730322ac4d5671b736574d44c
Ana Clara <ana4ew.br@gmail.com>
2024-11-15 16:15:20 -0300

    Fixed: Data relationship

--- src/main/java/com/example/web_inventory/services/OrderService.java
+++ src/main/java/com/example/web_inventory/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.example.web_inventory.services;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -9,7 +10,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.example.web_inventory.dtos.request.OrderRequestDTO;
+import com.example.web_inventory.entities.CustomerEntity;
 import com.example.web_inventory.entities.OrderEntity;
+import com.example.web_inventory.repositories.CustomerRepository;
 import com.example.web_inventory.repositories.OrderRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -20,9 +23,19 @@ public class OrderService {
     
     @Autowired
     private OrderRepository repository;
+
+    @Autowired
+    private CustomerRepository customerRepository;
     
     public OrderEntity createOrder(OrderRequestDTO dto) {
+        CustomerEntity customer = customerRepository.findById(dto.getCustomerId())
+            .orElseThrow(() -> new RuntimeException(
+                "Customer not found."
+            ));
+
         OrderEntity entity = new OrderEntity(dto);
+
+        entity.setCustomerId(customer);
 
         return repository.save(entity);
     }
@@ -42,7 +55,7 @@ public class OrderService {
     public ResponseEntity<OrderEntity> updateOrderById(OrderRequestDTO dto, Long  id) {
         return repository.findById(id)
         .map(update -> {
-            update.setDateTimeAtCreation(dto.getDateTimeAtCreation());
+            update.setDateTimeAtCreation(LocalDateTime.now());
             update.setStatus(dto.getStatus());
             OrderEntity updated = repository.save(update);
 
