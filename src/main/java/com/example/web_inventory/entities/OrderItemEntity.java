af35069b7beaf52731673e19f688cc179d766c63
Ana Clara <ana4ew.br@gmail.com>
2024-11-15 16:14:57 -0300

    Fixed: Data relationship

--- src/main/java/com/example/web_inventory/entities/OrderItemEntity.java
+++ src/main/java/com/example/web_inventory/entities/OrderItemEntity.java
@@ -12,7 +12,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -29,9 +28,8 @@ public class OrderItemEntity implements Serializable {
     @JoinColumn(name = "orders_id", nullable = false)
     private OrderEntity orderId;
 
-    @OneToMany
+    @ManyToOne
     @JoinColumn(name = "products_id", nullable = false)
-    @Column(nullable = false)
     private ProductEntity productId;
 
     @Column(nullable = false, length = 5)
