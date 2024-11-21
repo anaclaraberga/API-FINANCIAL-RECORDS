e36714c347d0618cf2e10662f2ea8e2b9e2626d2
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-11-20 23:01:14 -0300

    feat: add find by email and find by email and password

--- src/main/java/com/example/web_inventory/repositories/UserRepository.java
+++ src/main/java/com/example/web_inventory/repositories/UserRepository.java
@@ -1,11 +1,14 @@
 package com.example.web_inventory.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.web_inventory.entities.UserEntity;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, Long>{
-    
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+  Optional<UserEntity> findByEmail(String email);
+  Optional<UserEntity> findByEmailAndPassword(String email, String password);
 }
